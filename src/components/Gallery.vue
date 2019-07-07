5cafcf662c1b76c8a63c8c0938139478ed321a9f
Mostafa Khalid Raihan <mraihan29@gmail.com>
2019-07-07 13:13:49 +0600

    Initial commit

---
+++ src/components/Gallery.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="__gallery">
+    <GalleryItem v-for="i in 5" :key="i"/>
+  </div>
+</template>
+
+<script>
+import GalleryItem from "./GalleryItem";
+export default {
+  name: "Gallery",
+  props: {
+    // msg: String
+  },
+  components: {
+    GalleryItem
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.__gallery {
+  display: flex;
+  align-items: flex-start;
+  justify-content: start;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
---
+++ src/components/GalleryItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="__gallery-item"></div>
+</template>
+
+<script>
+export default {
+  name: "GalleryItem",
+  props: {
+    // msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.__gallery-item {
+  width: 30vw;
+  height: 30vh;
+  background-color: red;
+  margin: 2vw;
+}
+</style>
